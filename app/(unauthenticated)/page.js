77bf1c8992fc425e2fd9913d104d1ad16297b7e6
Kushagra Pandey <kushagra.15554@gmail.com>
2025-01-06 00:36:30 +0530

    final commit

--- app/(unauthenticated)/page.js
+++ app/(unauthenticated)/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import LandingPageImg from "@/assets/images/LandingPagephoto.jpg";
+import LandingPageImg from "../../assets/images/Landingpagephoto.jpg";
 export default function Home() {
   return (
     <>
